da34bd2837630c44bf8abd6131ad45ea20e05c00
fahmi <fahmiabdulaziz44@gmail.com>
2023-05-28 21:19:36 +0700

    test

---
+++ Flash Chat iOS13/Constans.swift
@@ -0,0 +1,29 @@
+//
+//  Constans.swift
+//  Flash Chat iOS13
+//
+//  Created by Fahmi Aziz on 05/04/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
+
---
+++ Flash Chat iOS13/Messages.swift
@@ -0,0 +1,12 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Fahmi Aziz on 05/04/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+struct Messages {
+    let sender: String
+    let body: String
+}
